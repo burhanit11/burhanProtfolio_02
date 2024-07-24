d1cf2a4a67f2699d32837ccdc35a7fcdbd031995
Burhan Rabbani <burhanit11@gmail.com>
2024-07-24 08:02:41 -0700

    download btn

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+// import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import CV from "../../assets/BurhanMernStackResume.pdf";
+
 const Media = () => {
+  const cvUrl = CV;
+
   return (
     <div className=" xl:flex-row gap-6 lgl:gap-0 justify-between">
       <div>
         {/* <h2 className="text-base uppercase font-titleFont mb-4">Download CV</h2> */}
         <div className="flex gap-4">
-          <button className="bannerIcon w-48">Download CV</button>
+       
+
+          <a
+            href={cvUrl}
+            className="bannerIcon w-48"
+            // download="BurhanMernStackResume.pdf"
+            target="_blank"
+          >
+            <button>Download CV</button>
+          </a>
+          {/* 
+          <button
+            className="bannerIcon w-48"
+            onclick={
+              handleOpenCV
+              // window.open("../../../public/BurhanMernStackResume.pdf")
+            }
+          >
+            Download CV
+          </button> */}
           <span className="bannerIcon">
             <FaFacebookF />
           </span>
